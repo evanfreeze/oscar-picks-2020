a61390e0ebdd414751ab38b18233b22b398221e8
Evan Freeze <evanfreeze@users.noreply.github.com>
2020-01-21 20:12:12 -0600

    Add links to stats and results on mobile

---
+++ src/components/Results.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Results = () => {
+    return (
+        <section>
+            <h2 id="results">Results</h2>
+            <p>No awards presented yet</p>
+        </section>
+    )
+}
+
+export default Results
---
+++ src/components/Stats.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Stats = () => {
+    return (
+        <section>
+            <h2 id="stats">Stats</h2>
+            <p>Some stats here</p>
+        </section>
+    )
+}
+
+export default Stats
